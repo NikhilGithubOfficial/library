848de35ea4d71a7ffbb1eb8481e086a724a0e885
Nikhil <nkmnikhil0009@gmail.com>
2021-09-30 23:08:32 +0530

    Updat app.js

--- app.js
+++ app.js
@@ -1,17 +1,19 @@
 let myLibrary = [];
 
 class Book {
-	constructor(title, author, pages, read) {
-		// the constructor...
-		this.title = title;
-		this.author = author;
-		this.pages = pages;
-		this.read = read;
-		this.info = function () {
-			if(read) return `${title} by ${author}, ${pages} pages, read.`;
-			else return `${title} by ${author}, ${pages} pages, not read yet.`;
-		};
-	}
+    constructor(title, author, pages, isRead) {
+        // the constructor...
+        this.title = title;
+        this.author = author;
+        this.pages = pages;
+        this.isRead = isRead;
+        this.info = function () {
+            if (isRead)
+                return `${title} by ${author}, ${pages} pages, isRead.`;
+            else
+                return `${title} by ${author}, ${pages} pages, not isRead yet.`;
+        };
+    }
 }
 
 function addBookToLibrary() {
